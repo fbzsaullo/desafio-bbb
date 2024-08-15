d47eb065221188e03066b48c74780ff02fee2c86
fbzsaullo <fabriziodsaullo@gmail.com>
2024-08-15 11:22:29 -0300

    created participants controller

--- voting-api/config/routes.rb
+++ voting-api/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
+  namespace :api do
+    resources :participants, only: [:index, :create, :destroy]
+  end
 end
