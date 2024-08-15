12776887f274c24abeb3f5af2c27e06d41c69ec3
fbzsaullo <fabriziodsaullo@gmail.com>
2024-08-15 16:43:50 -0300

    create vote job spec and fix votes controller spec

--- voting-api/spec/controller/votes_spec.rb
+++ voting-api/spec/controller/votes_spec.rb
@@ -5,12 +5,17 @@ RSpec.describe Api::VotesController, type: :controller do
   let!(:participant) { create(:participant) }
   let!(:contest_participant) { create(:contest_participant, contest: contest, participant: participant) }
 
+  before do
+    ActiveJob::Base.queue_adapter = :test
+  end
+
   describe "POST #create" do
-    it "creates a new vote" do
+    it "enqueues a VoteJob" do
       expect {
         post :create, params: { contest_id: contest.id, participant_id: participant.id }
-      }.to change(Vote, :count).by(1)
-      expect(response).to have_http_status(:created)
+      }.to have_enqueued_job(VoteJob).with(contest.id, participant.id)
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)["message"]).to eq("Vote has been cast")
     end
   end
 end
