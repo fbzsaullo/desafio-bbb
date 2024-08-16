ab77c0bc31a88d26d71a6e58fa647116c99cc3f2
fbzsaullo <fabriziodsaullo@gmail.com>
2024-08-16 02:43:58 -0300

    add responsive style

--- frontend/src/App.style.js
+++ frontend/src/App.style.js
@@ -18,6 +18,62 @@ h3 {
   font-size: 20px;
   font-weight: lighter;
 }
+
+@media (max-width: 1200px) {
+  h1 {
+    font-size: 40px;
+  }
+
+  h2 {
+    font-size: 22px;
+  }
+
+  h3 {
+    font-size: 18px;
+  }
+}
+
+@media (max-width: 992px) {
+  h1 {
+    font-size: 36px;
+  }
+
+  h2 {
+    font-size: 20px;
+  }
+
+  h3 {
+    font-size: 16px;
+  }
+}
+
+@media (max-width: 768px) {
+  h1 {
+    font-size: 32px;
+  }
+
+  h2 {
+    font-size: 18px;
+  }
+
+  h3 {
+    font-size: 14px;
+  }
+}
+
+@media (max-width: 576px) {
+  h1 {
+    font-size: 28px;
+  }
+
+  h2 {
+    font-size: 16px;
+  }
+
+  h3 {
+    font-size: 12px;
+  }
+}
 `;
 
 export default GlobalStyle;
