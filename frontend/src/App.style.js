ee5a6cf5c69a01efd3526fef40b0721349bc7496
fbzsaullo <fabriziodsaullo@gmail.com>
2024-08-15 22:12:58 -0300

    created active contest page

--- frontend/src/App.style.js
+++ frontend/src/App.style.js
@@ -2,7 +2,21 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 :root {
+  font-family: 'Inter', sans-serif;
   background-color: #F2F4FC;
+}
+
+h1 {
+  font-size: 48px;
+}
+
+h2 {
+  font-size: 24px;
+}
+
+h3 {
+  font-size: 20px;
+  font-weight: lighter;
 }
 `;
 
--- frontend/src/pages/ActiveContest/ActiveContest.syle.js
+++ frontend/src/pages/ActiveContest/ActiveContest.syle.js
@@ -1,5 +1,45 @@
 import styled from 'styled-components';
 
 export const ActiveContestStyle = styled.div`
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 80vh;
+  text-align: center;
+}
 
+.contest {
+  .participants {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+
+    .participant {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #83A0F4;
+      border-radius: 8px;
+      height: 86px;
+      width: 516px;
+      transition: 0.3s;
+
+      img {
+        height: 86px;
+        border-radius: 0 8px 8px 0;
+      }
+      h2 {
+        margin-left: 20px;
+      }
+      &:hover {
+        cursor: pointer;
+        filter: drop-shadow(0px 0px 10px #2D59DC);
+        transition: 0.3s;
+      }
+    }
+  }
+}
 `;
