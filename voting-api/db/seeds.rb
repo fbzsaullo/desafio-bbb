80a453ac173cd96b46de979e6a848f1c8fc17fd0
fbzsaullo <fabriziodsaullo@gmail.com>
2024-08-29 21:33:28 -0300

    seeds comment

--- voting-api/db/seeds.rb
+++ voting-api/db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# participants.each do |p|
+#   votes = rand(1000..5000)
+#     votes.times do
+#     (0..11).each do |i|
+#       Vote.create!(
+#         participant_id: p.id,
+#         contest_id:contest.id,
+#         created_at: i.hours.ago,
+#         updated_at: i.hours.ago,
+#       )
+#     end
+#   end
+# end
+  
